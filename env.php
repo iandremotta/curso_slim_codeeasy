eca9da316c691a266d8f45a7479d3f4e3e52f83e
André Motta <40709857+iandremotta@users.noreply.github.com>
2021-08-04 22:01:05 -0300

    Delete env.php

--- env.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    putenv('DISPLAY_ERRORS_DETAILS='. true);
-
-    putenv('GERENCIADOR_DE_LOJAS_HOST=localhost');
-    putenv('GERENCIADOR_DE_LOJAS_DBNAME=gerenciador_de_lojas');
-    putenv('GERENCIADOR_DE_LOJAS_USER=root');
-    putenv('GERENCIADOR_DE_LOJAS_PASSWORD=');
-    putenv('GERENCIADOR_DE_LOJAS_PORT=3306');
